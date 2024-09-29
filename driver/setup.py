fc099bcdcc2ab235c54cf5fde47c606dc96bc21d
mlzlzj <161783521+mlzlzj@users.noreply.github.com>
2024-09-29 08:37:09 +0800

    Add files via upload

--- driver/setup.py
+++ driver/setup.py
@@ -1,1 +1,25 @@
+from selenium import webdriver
 
+
+def setup_driver(proxy=None):
+    """
+    Setup the driver for selenium
+    """
+    options = webdriver.ChromeOptions()
+    options.add_argument("start-maximized")
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    options.add_experimental_option("useAutomationExtension", False)
+    options.add_argument("blink-settings=imagesEnabled=false")
+    options.add_argument("--log-level=3")
+    options.add_argument("--allow-running-insecure-content")
+    options.add_argument("blink-settings=imagesEnabled=false")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--window-position=-10000,-10000")
+    if proxy:
+        options.add_argument("--proxy-server=%s" % proxy)
+    driver = webdriver.Chrome(options=options)
+    return driver
---
+++ driver/utils.py
@@ -0,0 +1,46 @@
+from driver.setup import setup_driver
+from utils.retry import (
+    retry_func,
+    locate_element_with_retry,
+    find_clickable_element_with_retry,
+)
+from time import sleep
+import re
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+
+
+def get_soup_driver(url):
+    """
+    Get the soup by driver
+    """
+    driver = setup_driver()
+    retry_func(lambda: driver.get(url), name=url)
+    sleep(1)
+    source = re.sub(
+        r"<!--.*?-->",
+        "",
+        driver.page_source,
+        flags=re.DOTALL,
+    )
+    soup = BeautifulSoup(source, "html.parser")
+    driver.close()
+    driver.quit()
+    return soup
+
+
+def search_submit(driver, name):
+    """
+    Input key word and submit with driver
+    """
+    search_box = locate_element_with_retry(driver, (By.XPATH, '//input[@type="text"]'))
+    if not search_box:
+        return
+    search_box.clear()
+    search_box.send_keys(name)
+    submit_button = find_clickable_element_with_retry(
+        driver, (By.XPATH, '//input[@type="submit"]')
+    )
+    if not submit_button:
+        return
+    driver.execute_script("arguments[0].click();", submit_button)
