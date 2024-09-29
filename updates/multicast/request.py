dcd5f6545195c7d7b42acc5e9fbce3c24b537857
mlzlzj <161783521+mlzlzj@users.noreply.github.com>
2024-09-29 08:50:53 +0800

    Add files via upload

--- updates/multicast/request.py
+++ updates/multicast/request.py
@@ -1,1 +1,195 @@
+from utils.channel import (
+    get_results_from_multicast_soup,
+    get_results_from_multicast_soup_requests,
+    get_channel_multicast_name_region_type_result,
+    get_channel_multicast_region_type_list,
+    get_channel_multicast_result,
+    get_multicast_fofa_search_urls,
+)
+from utils.tools import get_pbar_remaining, get_soup, merge_objects
+from utils.config import config
+from updates.proxy import get_proxy, get_proxy_next
+from updates.fofa import get_channels_by_fofa
+from time import time
+from driver.setup import setup_driver
+from driver.utils import search_submit
+from utils.retry import (
+    retry_func,
+    find_clickable_element_with_retry,
+)
+from selenium.webdriver.common.by import By
+from tqdm.asyncio import tqdm_asyncio
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from requests_custom.utils import get_soup_requests, close_session
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
+from collections import defaultdict
+from .update_tmp import get_multicast_region_result_by_rtp_txt
 
+
+async def get_channels_by_multicast(names, callback=None):
+    """
+    Get the channels by multicase
+    """
+    channels = {}
+    pageUrl = "http://tonkiang.us/hoteliptv.php"
+    proxy = None
+    open_multicast_tonkiang = config.getboolean("Settings", "open_multicast_tonkiang")
+    open_multicast_fofa = config.getboolean("Settings", "open_multicast_fofa")
+    open_proxy = config.getboolean("Settings", "open_proxy")
+    open_driver = config.getboolean("Settings", "open_driver")
+    page_num = config.getint("Settings", "multicast_page_num")
+    if open_proxy:
+        proxy = await get_proxy(pageUrl, best=True, with_test=True)
+    multicast_region_result = get_multicast_region_result_by_rtp_txt(callback=callback)
+    name_region_type_result = get_channel_multicast_name_region_type_result(
+        multicast_region_result, names
+    )
+    region_type_list = get_channel_multicast_region_type_list(name_region_type_result)
+    search_region_type_result = defaultdict(lambda: defaultdict(list))
+    if open_multicast_fofa:
+        fofa_search_urls = get_multicast_fofa_search_urls()
+        fofa_result = await get_channels_by_fofa(
+            fofa_search_urls, multicast=True, callback=callback
+        )
+        merge_objects(search_region_type_result, fofa_result)
+
+    def process_channel_by_multicast(region, type):
+        nonlocal proxy, open_driver, page_num, start_time
+        name = f"{region}{type}"
+        info_list = []
+        driver = None
+        try:
+            if open_driver:
+                driver = setup_driver(proxy)
+                try:
+                    retry_func(
+                        lambda: driver.get(pageUrl), name=f"multicast search:{name}"
+                    )
+                except Exception as e:
+                    if open_proxy:
+                        proxy = get_proxy_next()
+                    driver.close()
+                    driver.quit()
+                    driver = setup_driver(proxy)
+                    driver.get(pageUrl)
+                search_submit(driver, name)
+            else:
+                page_soup = None
+                post_form = {"saerch": name}
+                code = None
+                try:
+                    page_soup = retry_func(
+                        lambda: get_soup_requests(pageUrl, data=post_form, proxy=proxy),
+                        name=f"multicast search:{name}",
+                    )
+                except Exception as e:
+                    if open_proxy:
+                        proxy = get_proxy_next()
+                    page_soup = get_soup_requests(pageUrl, data=post_form, proxy=proxy)
+                if not page_soup:
+                    print(f"{name}:Request fail.")
+                    return {"region": region, "type": type, "data": info_list}
+                else:
+                    a_tags = page_soup.find_all("a", href=True)
+                    for a_tag in a_tags:
+                        href_value = a_tag["href"]
+                        parsed_url = urlparse.urlparse(href_value)
+                        code = parse_qs(parsed_url.query).get("code", [None])[0]
+                        if code:
+                            break
+            for page in range(1, page_num + 1):
+                try:
+                    if page > 1:
+                        if open_driver:
+                            page_link = find_clickable_element_with_retry(
+                                driver,
+                                (
+                                    By.XPATH,
+                                    f'//a[contains(@href, "={page}") and contains(@href, "{name}")]',
+                                ),
+                            )
+                            if not page_link:
+                                break
+                            driver.execute_script("arguments[0].click();", page_link)
+                        else:
+                            request_url = (
+                                f"{pageUrl}?net={name}&page={page}&code={code}"
+                            )
+                            page_soup = retry_func(
+                                lambda: get_soup_requests(request_url, proxy=proxy),
+                                name=f"multicast search:{name}, page:{page}",
+                            )
+                    soup = get_soup(driver.page_source) if open_driver else page_soup
+                    if soup:
+                        if "About 0 results" in soup.text:
+                            break
+                        results = (
+                            get_results_from_multicast_soup(soup)
+                            if open_driver
+                            else get_results_from_multicast_soup_requests(soup)
+                        )
+                        print(name, "page:", page, "results num:", len(results))
+                        if len(results) == 0:
+                            print(f"{name}:No results found")
+                        info_list = info_list + results
+                    else:
+                        print(f"{name}:No page soup found")
+                        if page != page_num and open_driver:
+                            driver.refresh()
+                except Exception as e:
+                    print(f"{name}:Error on page {page}: {e}")
+                    continue
+        except Exception as e:
+            print(f"{name}:Error on search: {e}")
+            pass
+        finally:
+            if driver:
+                driver.close()
+                driver.quit()
+            pbar.update()
+            if callback:
+                callback(
+                    f"正在进行Tonkiang组播更新, 剩余{region_type_list_len - pbar.n}个地区待查询, 预计剩余时间: {get_pbar_remaining(n=pbar.n, total=pbar.total, start_time=start_time)}",
+                    int((pbar.n / region_type_list_len) * 100),
+                )
+            return {"region": region, "type": type, "data": info_list}
+
+    if open_multicast_tonkiang:
+        region_type_list_len = len(region_type_list)
+        pbar = tqdm_asyncio(total=region_type_list_len, desc="Multicast search")
+        if callback:
+            callback(
+                f"正在进行Tonkiang组播更新, {len(names)}个频道, 共{region_type_list_len}个地区",
+                0,
+            )
+        start_time = time()
+        with ThreadPoolExecutor(max_workers=3) as executor:
+            futures = {
+                executor.submit(process_channel_by_multicast, region, type): (
+                    region,
+                    type,
+                )
+                for region, type in region_type_list
+            }
+
+            for future in as_completed(futures):
+                region, type = futures[future]
+                result = future.result()
+                data = result.get("data")
+
+                if data:
+                    for item in data:
+                        url = item.get("url")
+                        date = item.get("date")
+                        if url:
+                            search_region_type_result[region][type].append(
+                                (url, date, None)
+                            )
+        pbar.close()
+    channels = get_channel_multicast_result(
+        name_region_type_result, search_region_type_result
+    )
+    if not open_driver:
+        close_session()
+    return channels
---
+++ updates/multicast/update_tmp.py
@@ -0,0 +1,167 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
+from updates.subscribe import get_channels_by_subscribe_urls
+from driver.utils import get_soup_driver
+from utils.config import resource_path, config
+from utils.channel import format_channel_name
+from utils.tools import get_pbar_remaining
+import json
+
+# import asyncio
+from requests import Session
+from collections import defaultdict
+import re
+from time import time
+from tqdm import tqdm
+
+
+def get_region_urls_from_IPTV_Multicast_source():
+    """
+    Get the region urls from IPTV_Multicast_source
+    """
+    region_url = {}
+    origin_url = "https://github.com/xisohi/IPTV-Multicast-source/blob/main/README.md"
+    soup = get_soup_driver(origin_url)
+    tbody = soup.find("tbody")
+    trs = tbody.find_all("tr") if tbody else []
+    for tr in trs:
+        tds = tr.find_all("td")
+        name = tds[0].get_text().strip()
+        unicom = tds[1].find("a", href=True).get("href")
+        mobile = tds[2].find("a", href=True).get("href")
+        telecom = tds[3].find("a", href=True).get("href")
+        if name not in region_url:
+            region_url[name] = {}
+        region_url[name]["联通"] = unicom
+        region_url[name]["移动"] = mobile
+        region_url[name]["电信"] = telecom
+    with open(
+        resource_path("updates/multicast/multicast_map.json"), "w", encoding="utf-8"
+    ) as f:
+        json.dump(region_url, f, ensure_ascii=False, indent=4)
+
+
+def get_multicast_urls_info_from_region_list():
+    """
+    Get the multicast urls info from region
+    """
+    urls_info = []
+    with open(
+        resource_path("updates/multicast/multicast_map.json"), "r", encoding="utf-8"
+    ) as f:
+        region_url = json.load(f)
+        urls_info = [
+            {"region": region, "type": type, "url": url}
+            for region, value in region_url.items()
+            for type, url in value.items()
+        ]
+        return urls_info
+
+
+async def get_multicast_region_result():
+    """
+    Get multicast region result
+    """
+    multicast_region_urls_info = get_multicast_urls_info_from_region_list()
+    multicast_result = await get_channels_by_subscribe_urls(
+        multicast_region_urls_info, multicast=True
+    )
+    with open(
+        resource_path("updates/multicast/multicast_region_result.json"),
+        "w",
+        encoding="utf-8",
+    ) as f:
+        json.dump(multicast_result, f, ensure_ascii=False, indent=4)
+
+
+def get_multicast_region_type_result_txt():
+    """
+    Get multicast region type result txt
+    """
+    with open(
+        resource_path("updates/multicast/multicast_map.json"), "r", encoding="utf-8"
+    ) as f:
+        region_url = json.load(f)
+        session = Session()
+        for region, value in region_url.items():
+            for type, url in value.items():
+                response = session.get(url)
+                content = response.text
+                with open(
+                    resource_path(f"config/rtp/{region}_{type}.txt"),
+                    "w",
+                    encoding="utf-8",
+                ) as f:
+                    f.write(content)
+
+
+def get_multicast_region_result_by_rtp_txt(callback=None):
+    """
+    Get multicast region result by rtp txt
+    """
+    rtp_path = resource_path("config/rtp")
+    config_region_list = set(
+        region.strip()
+        for region in config.get("Settings", "multicast_region_list").split(",")
+        if region.strip()
+    )
+    rtp_file_list = [
+        filename.rsplit(".", 1)[0]
+        for filename in os.listdir(rtp_path)
+        if filename.endswith(".txt")
+        and "_" in filename
+        and (
+            filename.rsplit(".", 1)[0].split("_", 1)[0] in config_region_list
+            or config_region_list & {"all", "ALL", "全部"}
+        )
+    ]
+
+    total_files = len(rtp_file_list)
+    if callback:
+        callback(f"正在读取本地组播数据, 共{total_files}个文件", 0)
+
+    pbar = tqdm(total=total_files, desc="Loading local multicast rtp files")
+    multicast_result = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+    pattern = re.compile(r"^(.*?),(?!#genre#)(.*?)$")
+    start_time = time()
+
+    for filename in rtp_file_list:
+        region, type = filename.split("_", 1)
+        with open(
+            os.path.join(rtp_path, f"{filename}.txt"), "r", encoding="utf-8"
+        ) as f:
+            for line in f:
+                matcher = pattern.match(line)
+                if matcher:
+                    channel_name = format_channel_name(matcher.group(1).strip())
+                    url = matcher.group(2).strip()
+                    if url not in multicast_result[channel_name][region][type]:
+                        multicast_result[channel_name][region][type].append(url)
+        pbar.update()
+        if callback:
+            remaining_files = total_files - pbar.n
+            estimated_time = get_pbar_remaining(pbar.n, total_files, start_time)
+            callback(
+                f"正在读取{region}_{type}的组播数据, 剩余{remaining_files}个文件, 预计剩余时间: {estimated_time}",
+                int((pbar.n / total_files) * 100),
+            )
+
+    with open(
+        resource_path("updates/multicast/multicast_region_result.json"),
+        "w",
+        encoding="utf-8",
+    ) as f:
+        json.dump(multicast_result, f, ensure_ascii=False, indent=4)
+
+    pbar.close()
+    return multicast_result
+
+
+if __name__ == "__main__":
+    get_region_urls_from_IPTV_Multicast_source()
+    # asyncio.run(get_multicast_region_result())
+    get_multicast_region_type_result_txt()
+    # get_multicast_region_result_by_rtp_txt()
