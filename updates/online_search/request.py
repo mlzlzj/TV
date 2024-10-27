8dddedc57afa708076dc15dcc686293272fc639f
mlzlzj <161783521+mlzlzj@users.noreply.github.com>
2024-10-27 19:46:42 +0800

    Add files via upload

--- updates/online_search/request.py
+++ updates/online_search/request.py
@@ -25,18 +25,20 @@ from tqdm.asyncio import tqdm_asyncio
 from concurrent.futures import ThreadPoolExecutor
 from requests_custom.utils import get_soup_requests, close_session
 
+timeout = config.getint("Settings", "request_timeout", fallback=10)
+
 
 async def use_accessible_url(callback):
     """
     Check if the url is accessible
     """
-    callback(f"正在获取最优的在线检索节点", 0)
+    callback(f"正在获取最优的关键字搜索节点", 0)
     baseUrl1 = "https://www.foodieguide.com/iptvsearch/"
     baseUrl2 = "http://tonkiang.us/"
-    task1 = create_task(get_speed(baseUrl1, timeout=30))
-    task2 = create_task(get_speed(baseUrl2, timeout=30))
+    task1 = create_task(get_speed(baseUrl1, timeout=timeout))
+    task2 = create_task(get_speed(baseUrl2, timeout=timeout))
     task_results = await gather(task1, task2)
-    callback(f"获取在线检索节点完成", 100)
+    callback(f"获取关键字搜索节点完成", 100)
     if task_results[0] == float("inf") and task_results[1] == float("inf"):
         return None
     if task_results[0] < task_results[1]:
@@ -55,9 +57,9 @@ async def get_channels_by_online_search(names, callback=None):
     if not pageUrl:
         return channels
     proxy = None
-    open_proxy = config.getboolean("Settings", "open_proxy")
-    open_driver = config.getboolean("Settings", "open_driver")
-    page_num = config.getint("Settings", "online_search_page_num")
+    open_proxy = config.getboolean("Settings", "open_proxy", fallback=False)
+    open_driver = config.getboolean("Settings", "open_driver", fallback=True)
+    page_num = config.getint("Settings", "online_search_page_num", fallback=3)
     if open_proxy:
         proxy = await get_proxy(pageUrl, best=True, with_test=True)
     start_time = time()
