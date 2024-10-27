66e925e57d1388cfc399d1e2381379aaa0fa7e97
mlzlzj <161783521+mlzlzj@users.noreply.github.com>
2024-10-27 19:45:14 +0800

    Add files via upload

--- updates/fofa/fofa_map.py
+++ updates/fofa/fofa_map.py
@@ -1,57 +1,4 @@
 region_url = {
-    "梅 州": [
-        "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0ibWVpemhvdSI%3D",
-        "https://www.zoomeye.org/searchResult?q=city%3A%22meizhou%22",
-    ],
-    "长 沙": [
-        "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIGNpdHk9ImNoYW5nc2hhIg%3D%3D",
-        "https://www.zoomeye.org/searchResult?q=city:%22changsha%22",
-    ],
-    "株 洲": [
-        "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iemh1emhvdSI%3D",
-        "https://www.zoomeye.org/searchResult?q=city%3A%22zhuzhou%22",
-    ],
-    "衡 阳": [
-        "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iaGVuZ3lhbmci",
-        "https://www.zoomeye.org/searchResult?q=city%3A%22hengyang%22",
-    ],
-    "郴 州": [
-        "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iY2hlbnpob3Ui",
-        "https://www.zoomeye.org/searchResult?q=city%3A%22chenzhou%22",
-    ],
-    "湘 潭": [
-        "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0ieGlhbmd0YW4i",
-        "https://www.zoomeye.org/searchResult?q=city%3A%22xiangtan%22",
-    ],
-    "常 德": [
-        "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iY2hhbmdkZSI%3D",
-        "https://www.zoomeye.org/searchResult?q=city%3A%22changde%22",
-    ],
-    "益 阳": [
-        "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0ieWl5YW5nIg%3D%3D",
-        "https://www.zoomeye.org/searchResult?q=city%3A%22yiyang%22",
-    ],
-    "永 州": [
-        "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0ieW9uZ3pob3Ui",
-        "https://www.zoomeye.org/searchResult?q=city%3A%22yongzhou%22",
-    ],
-    "怀 化": [
-        "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iaHVhaWh1YSI%3D",
-        "https://www.zoomeye.org/searchResult?q=city%3A%22huaihua%22",
-    ],
-    "邵 阳": [
-        "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0ic2hhb3lhbmci",
-        "https://www.zoomeye.org/searchResult?q=city%3A%22shaoyang%22",
-    ],
-    "娄 底": [
-        "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0ibG91ZGki",
-        "https://www.zoomeye.org/searchResult?q=city%3A%22loudi%22",
-    ],
-    "张家界": [
-        "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iemhhbmdqaWFqaWUi",
-        "https://www.zoomeye.org/searchResult?q=city%3A%22zhangjiajie%22",
-    ],
-
     "广东": [
         "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iR3Vhbmdkb25nIg%3D%3D",
         "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22guangdong%22",
@@ -133,6 +80,10 @@ region_url = {
     "四川": [
         "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iU2ljaHVhbiI%3D",
         "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22sichuan%22",
+    ],
+    "云南": [
+        "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iWXVubmFuIg%3D%3D",
+        "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22yunnan%22",
     ],
     "香港": [
         "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iSEsi"
